319e805611422a889c2086d5673220a454938809
Still11 <misgana21son@gmial.com>
2023-07-07 02:45:10 +0200

    first commit

---
+++ vrWeb/src/App.tsx
@@ -0,0 +1,13 @@
+import ProjectClever from "./Components/ProjectClever";
+import ProjectsList from "./Components/ProjectsList";
+import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      <ProjectClever />
+    </div>
+  );
+}
+
+export default App;
